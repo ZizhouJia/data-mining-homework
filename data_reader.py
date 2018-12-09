a07e3d2a9b74503cbdd4da6c9f683ac17527a1f8
ZiZhouJia <jiazizhou@126.com>
2018-12-09 15:02:47 +0800

    fix the logist regression

--- data_reader.py
+++ data_reader.py
@@ -14,6 +14,34 @@ def process_data(data_str,dict=None):
         return label
 
 
+def process_number_row(row,division_part=10):
+    data=np.zeros((len(row),division_part))
+    thresh=np.zeros(division_part+1)
+    min_number=data.min()
+    distance=data.max()-data.min()
+    distance=distance/division_part
+    for i in range(0,11):
+        thresh[i]=min_number+distance*i
+    for j in range(0,len(row)):
+        c_data=row[j]
+        for i in range(0,10):
+            if(c_data>=thresh[i] and c_data<thresh[i+1]):
+                c_data[j,i]=1.0
+                break
+    return data
+
+
+def process_number_to_label(matrix,division_part=10):
+    logit_matrix=None
+    for i in range(0,matrix.shape[1]):
+        data=process_number_row(matrix[:,i])
+        if(logit_matrix is None):
+            logit_matrix=data
+        else:
+            logit_matrix=np.concatenate((logit_matrix,data),axis=1)
+
+    return logit_matrix
+
 
 def get_dict(file_path,key,csv_split):
     file=open(file_path,'rb')
@@ -26,6 +54,7 @@ def get_dict(file_path,key,csv_split):
     file.close()
     return dict
 
+
 def process_colume(file_path,dict,key,csv_split):
     file=open(file_path,'rb')
     reader=csv.DictReader(file,delimiter=csv_split)
@@ -35,6 +64,7 @@ def process_colume(file_path,dict,key,csv_split):
     colume=np.array(colume)
     file.close()
     return colume
+
 
 def process_all(reader,value_key_list,string_key_list,csv_split=";"):
     matrix=None
@@ -54,11 +84,13 @@ def process_all(reader,value_key_list,string_key_list,csv_split=";"):
             matrix=np.concatenate((matrix,colume),axis=1)
     return matrix
 
+
 def read_bank_data(file_path="bank-additional.csv"):
     value_key_list=['age','duration','campaign','pdays','previous','emp.var.rate','cons.price.idx','euribor3m','nr.employed']
     string_key_list=['job','marital','education','default','housing','loan','contact','month','day_of_week','poutcome','y']
     matrix=process_all(file_path,value_key_list,string_key_list)
     return matrix[:,:-1],matrix[:,-1:]
+
 
 def read_frog_data(file_path="Frogs_MFCCs.csv"):
     value_key_list=[]
@@ -78,6 +110,7 @@ def read_frog_data(file_path="Frogs_MFCCs.csv"):
     key_label=["RecordID"]
     label_record=process_all(file_path,[],key_label,csv_split=",")
     return data,label_family,label_genus,label_species,label_record
+
 
 if __name__ == '__main__':
     data,label=read_bank_data()
---
+++ logistic_regression.py
@@ -0,0 +1,120 @@
+import numpy as np
+import data_reader
+
+class bank_dataset:
+    def __init__(self):
+        self.data,self.label=data_reader.read_bank_data()
+        data_part1=data_reader.process_number_to_label(self.data[:,0:9])
+        data_part2=self.data[:,9:]
+        print(self.data.shape)
+        print(data_part1.shape)
+        print(data_part2.shape)
+        self.data=np.concatenate((data_part1,data_part2),axis=1)
+        print(self.data.shape)
+        self.label=np.reshape(self.label,(-1))
+        self.data_neg=self.data[self.label<0.5]
+        self.data_pos=self.data[self.label>0.5]
+        self.data_pos=np.repeat(self.data_pos,int(len(self.data_neg)/len(self.data_pos)),axis=0)
+        self.label_pos=np.ones(len(self.data_pos))
+        self.label_neg=np.zeros(len(self.data_neg))
+        self.data=np.concatenate((self.data_pos,self.data_neg),axis=0)
+        self.label=np.concatenate((self.label_pos,self.label_neg),axis=0)
+        state=np.random.get_state()
+        np.random.shuffle(self.data)
+        np.random.set_state(state)
+        np.random.shuffle(self.label)
+        # self.data=self.normalize(self.data)
+        self.train_X=[]
+        self.train_Y=[]
+        self.test_X=[]
+        self.test_Y=[]
+        for i in range(0,len(self.data)):
+            if(i%10>=0 and i%10<=6):
+                self.train_X.append(self.data[i])
+                self.train_Y.append(self.label[i])
+            else:
+                self.test_X.append(self.data[i])
+                self.test_Y.append(self.label[i])
+        self.train_X=np.array(self.train_X)
+        self.test_X=np.array(self.test_X)
+        self.train_Y=np.array(self.train_Y)
+        self.test_Y=np.array(self.test_Y)
+        self.index=0
+
+    def next_batch(self,batch_size=32):
+        if(self.index+batch_size<len(self.train_X)):
+            begin=self.index
+            self.index+=batch_size
+            return self.train_X[begin:self.index],self.train_Y[begin:self.index],False
+        else:
+            self.index=batch_size
+            return self.train_X[0:self.index],self.train_Y[0:self.index],True
+
+    def get_test_set(self):
+        return self.test_X,self.test_Y
+
+    def get_input_dim(self):
+        return self.train_X.shape[1]
+
+    def normalize(self,v):
+        e=1e-8
+        # mean=np.mean(v[:,0:9],axis=0)
+        # std=np.std(v[:,0:9],axis=0,ddof=1)
+        # v[:,0:9]=(v[:,0:9]-mean)/(std+e)
+        mean=np.mean(v,axis=0)
+        std=np.std(v,axis=0,ddof=1)
+        v=(v-mean)/(std+e)
+        return v
+
+
+class logistic_regression:
+    def __init__(self,input_dim):
+        self.W=np.random.randn(input_dim,1)/100
+        self.b=0.0
+
+    def predict(self,input):
+        output=np.dot(input,self.W)+self.b
+        output=1.0/(1.0+np.exp(-output))
+        return output
+
+    def train(self,input,label,learning_rate=0.0001,l2=1.0):
+        label=np.reshape(label,(-1,1))
+        batch_size=input.shape[0]
+        output=self.predict(input)
+        cha=label-output
+        self.W=self.W*(1-learning_rate*l2/batch_size)+learning_rate*np.sum(cha*input,axis=0)/batch_size
+        self.b=self.b*(1-learning_rate*l2/batch_size)+learning_rate*np.sum(cha)/batch_size
+
+    def calculate_acc(self,input,label):
+        label=np.reshape(label,(-1,1))
+        output=self.predict(input)
+        output[output>0.5]=1.0
+        output[output<0.5]=0.0
+        sum=np.sum((output==label).astype(np.float32))
+        return sum/label.shape[0]
+
+    def calculate_loss(self,input,label):
+        label=np.reshape(label,(-1,1))
+        output=self.predict(input)
+        e=1e-8
+        loss=-label*np.log(output+e)-(1-label)*np.log(1-output+e)
+        loss2=np.mean(self.W*self.W)
+        return np.mean(loss),loss2
+
+
+
+
+if __name__ == '__main__':
+    dataset=bank_dataset()
+    classifier=logistic_regression(dataset.get_input_dim())
+    epoch=100
+    iteration=1
+    for i in range(0,epoch):
+        epoch_end_mark=False
+        while(not epoch_end_mark):
+            data,label,epoch_end_mark=dataset.next_batch()
+            classifier.train(data,label)
+        test_data,test_label=dataset.get_test_set()
+        acc=classifier.calculate_acc(test_data,test_label)
+        loss,loss2=classifier.calculate_loss(test_data,test_label)
+        print("in epoch %d the acc is: %.4f the loss is: %.4f l2 loss is: %.4f"%(i,acc,loss,loss2))
