e44b69c6e84a99ea4a45cb261fc42c8073139a60
ZiZhouJia <jiazizhou@126.com>
2018-12-09 16:29:33 +0800

    add evaluation

--- k-means.py
+++ k-means.py
@@ -4,10 +4,10 @@ import data_reader
 import evaluation
 
 class k_means:
-    def __init__(self,data,label,centers=4):
+    def __init__(self,data,label,centers=40):
         self.data=data
-        self.label=label.astype(np.int32)
-        self.center=np.zeros(centers,self.data.shape[1])
+        self.label=label
+        self.center=np.zeros((centers,self.data.shape[1]))
 
     def init_center(self):
         div=len(self.data)/len(self.center)
@@ -24,43 +24,44 @@ class k_means:
         self.new_center=np.zeros(self.center.shape)
         for i in range(0,len(self.data)):
             min_index=0
-            min_value=-1.0
+            min_value=1e100
             for j in range(0,len(self.center)):
                 square=self.distance(self.center[j],self.data[i])
                 if(square<min_value):
                     min_index=j
                     min_value=square
-                self.new_center[min_index]+=self.data[i]
-                self.count[min_index]+=1
-        self.new_center=self.new_center/self.count
+            self.new_center[min_index]+=self.data[i]
+            self.count[min_index]+=1
+        self.new_center=self.new_center/self.count.reshape((-1,1))
         self.center=self.new_center
 
     def predict_data(self):
         predict_index=np.zeros(len(self.data)).astype(np.int32)
         for i in range(0,len(self.data)):
             min_index=0
-            min_value=-1.0
+            min_value=1e100
             for j in range(0,len(self.center)):
                 square=self.distance(self.center[j],self.data[i])
                 if(square<min_value):
                     min_index=j
                     min_value=square
-                predict_index[i]=min_index
+            predict_index[i]=min_index
 
-        new_predict=np.array(self.label.shape)
-        for i in range(0,len(self.center)):
-            self.current_label=self.label[predict_index==i]
-            count = np.bincount(self.current_label)
-            new_predict[predict_index==i]=np.argwhere(count=np.max(count))[0]
-        return new_predict
+        return predict_index
 
 if __name__ == '__main__':
+    clusters=100
     data,label_family,label_genus,label_species,label_record=data_reader.read_frog_data()
-    predictor=k_means(data,label_family)
+    label=np.argmin(-label_family,axis=1)
+    predictor=k_means(data,label,clusters)
     predictor.init_center()
     iteration=100
     for i in range(0,iteration):
         predictor.update()
-        perdict=predictor.predict_data()
-        acc=evaluation.accuracy(predict,label_family)
-        print("the acc in it &d is %.4f"%(i,acc))
+        predict=predictor.predict_data()
+        acc=evaluation.multi_label_accuracy(predict,label,clusters)
+        p=evaluation.purity(predict,label,clusters)
+        F_score=evaluation.F_score(predict,label,clusters)
+        F_score_output=evaluation.format_F_score(F_score)
+        print("the acc in it %d is %.4f %.4f"%(i,acc,p))
+        print("the Fscore is "+F_score_output)
